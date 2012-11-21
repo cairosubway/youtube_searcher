5ea85c1e42a4a58084009428625fd0a7687b5b5c
arh444 <arh444@arh444-HP-Pavilion-dv5000-EE801AV.(none)>
2012-11-21 12:34:37 -0500

    second commit

--- searcher1/gen/com/example/searcher1/R.java
+++ searcher1/gen/com/example/searcher1/R.java
@@ -45,16 +45,14 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int combinedtabandlist=0x7f050006;
-        public static final int hello_world=0x7f050001;
-        public static final int json_parser=0x7f050004;
-        public static final int json_parser1=0x7f050005;
-        public static final int menu_settings=0x7f050002;
-        public static final int search=0x7f050009;
-        public static final int searchYouTube=0x7f05000a;
+        public static final int combinedtabandlist=0x7f050004;
+        public static final int json_parser=0x7f050003;
+        public static final int menu_settings=0x7f050001;
+        public static final int search=0x7f050006;
+        public static final int searchYouTube=0x7f050007;
         public static final int title=0x7f050008;
-        public static final int title_activity_main=0x7f050003;
-        public static final int title_activity_search=0x7f050007;
+        public static final int title_activity_main=0x7f050002;
+        public static final int title_activity_search=0x7f050005;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
